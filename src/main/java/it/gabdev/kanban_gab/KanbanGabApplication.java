97969a74b49f357e255c64e29ad9788e3432efff
GabScognamiglio <gabbo16s@gmail.com>
2024-10-01 15:00:09 +0200

    initial commit

---
+++ src/main/java/it/gabdev/kanban_gab/KanbanGabApplication.java
@@ -0,0 +1,13 @@
+package it.gabdev.kanban_gab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanGabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanGabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gabdev/kanban_gab/KanbanGabApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gabdev.kanban_gab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanGabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
